3596e0b6ee485aa497c2a3e03bd66474f1f88bc2
Joceline Rios <99524459+JocieRios@users.noreply.github.com>
2024-03-25 01:52:54 -0400

    Module 11 HW
    Module # 11 Debugging and defensive programming in R

---
+++ Rios_HW11.R
@@ -0,0 +1,23 @@
+# Bugged Function
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } return(outlier.vec) } # Return value should not be inline with the assignment
+
+# Debugged Function
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } 
+  return(outlier.vec) } # Return value was moved to next line
+
